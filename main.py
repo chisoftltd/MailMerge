aa34c51d32696ea2ed6de1cac15458bc82149e7b
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 01:45:40 +0000

    MailMerge

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-names_list = []
-with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Names/invited_names.txt") as f:
-    names_list.append(f.read())
+with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Names/invited_names.txt") as fname:
+    names_list = fname.readline().rsplit()
 
-for name in names_list:
-    with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Letters/starting_letter.txt") as f:
-        letter = f.read()
-        letter = letter.replace('[name]', name)
-        print(letter)
-
-    with open(f"/Users/benjamin.chinwe/PycharmProjects/MailMerge/Output/ReadyToSend/{name}.txt") as f:
-        f.write(letter)
+    while names_list:
+        with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Letters/starting_letter.txt") as fletter:
+            letter = fletter.read()
+            letter = letter.replace('[name]', fname.readline().replace("\n", ""))
+            print(letter)
+            name = fname.readline().replace("\n", "")
+        with open(f"/Users/benjamin.chinwe/PycharmProjects/MailMerge/Output/ReadyToSend/{name}.txt", mode="w") as f:
+            f.write(letter)
