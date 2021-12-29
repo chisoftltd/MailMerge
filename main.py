dfc84a45c4741be5a4dbb1940b1b6b1a83f641e6
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 11:01:06 +0000

    Using relative path

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 #   Get the guest list from file
-with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Names/invited_names.txt") as fname:
+with open("./Input/Names/invited_names.txt") as fname:
     names_list = fname.readlines()
 
 #   Get invite letter template from file
-with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Letters/starting_letter.txt") as fletter:
+with open("./Input/Letters/starting_letter.txt") as fletter:
     letter = fletter.read()
 
 #   Insert invitees name into temple letter
@@ -13,7 +13,7 @@ for name in names_list:
     inviteLetter = letter.replace('[name]', f"{name.strip()}")
 
     # Write the invite letters to file
-    with open(f"/Users/benjamin.chinwe/PycharmProjects/MailMerge/Output/ReadyToSend/{name.strip()}.txt", mode="w") as f:
+    with open(f"./Output/ReadyToSend/{name.strip()}.txt", mode="w") as f:
         print(inviteLetter)
         f.write(inviteLetter)
 
