57f60bd0de18cb6e3043d43aa573a40808292466
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 08:56:16 +0000

    Complete MailMerge

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Names/invited_names.txt") as fname:
-    names_list = fname.readline().rsplit()
 
-    while names_list:
-        with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Letters/starting_letter.txt") as fletter:
-            letter = fletter.read()
-            letter = letter.replace('[name]', fname.readline().replace("\n", ""))
-            print(letter)
-            name = fname.readline().replace("\n", "")
-        with open(f"/Users/benjamin.chinwe/PycharmProjects/MailMerge/Output/ReadyToSend/{name}.txt", mode="w") as f:
-            f.write(letter)
+#   Get the guest list from file
+with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Names/invited_names.txt") as fname:
+    names_list = fname.read().rsplit()
+
+#   Get invite letter template from file
+with open("/Users/benjamin.chinwe/PycharmProjects/MailMerge/Input/Letters/starting_letter.txt") as fletter:
+    letter = fletter.read()
+
+#   Insert invitees name into temple letter
+inviteLetter = []
+for name in names_list:
+    print(f"{name}")
+    inviteLetter = letter.replace('[name]', f"{name}")
+
+    # Write the invite letters to file
+    with open(f"/Users/benjamin.chinwe/PycharmProjects/MailMerge/Output/ReadyToSend/{name}.txt", mode="w") as f:
+        print(inviteLetter)
+        f.write(inviteLetter)
+
